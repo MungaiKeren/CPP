356559d6085d0d6a1a2a7ce128b6eddf1f027f5f
MungaiKeren <wambukeren@gmail.com>
2021-08-19 20:36:57 +0300

    did a few string methods

--- strings.cpp
+++ strings.cpp
@@ -10,4 +10,13 @@ int main()
 
     int len;
     strcpy(str3, str1);
+    cout<<"strcpy(str3, str1) - copied str1 to str3 " << str3 << endl<<endl;
+
+    strcat(str1, str2);
+    cout<<"strcat, joins two strings and stores the result in the first string."<<endl;
+    cout<< str1 <<endl<<endl;
+
+    len = strlen(str1);
+    cout<<"The length of " <<str1 <<" is "<< len <<endl;
+    return 0;
 };
