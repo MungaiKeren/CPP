7fa8c0d9543495bfae68facc447510c4af89e19b
MungaiKeren <wambukeren@gmail.com>
2021-08-19 21:49:07 +0300

    used the sqrt inbuilt function

--- inbuiltFuncs.cpp
+++ inbuiltFuncs.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+// include the header file
+#include <cmath>
 
 using namespace std;
 int main()
 {
-    cout <<"You could print something here" <<endl
+    double number, squareroot;
+    number = 25.0;
+    squareroot = sqrt(number);
+
+    cout <<"The squareroot of "<< number<<" is "<< squareroot <<endl;
 };
